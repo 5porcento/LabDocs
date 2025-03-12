765915bc1e92951e930b120673eecfd0958e9be3
Kauan Pacheco <kauanpachequinho122@gmail.com>
2025-03-11 22:30:27 -0300

    Adição dos menus

--- src/main/java/com/example/labdocs/HelloApplication.java
+++ src/main/java/com/example/labdocs/HelloApplication.java
@@ -2,21 +2,17 @@ package com.example.labdocs;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
-        Parent root = fxmlLoader.load(); // Carrega o FXML
-
-        Scene scene = new Scene(root, 400, 300);
-        scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm()); // Aplica CSS
-
-        stage.setTitle("JavaFX com FXML");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("menu-inicial.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 400, 300);
+        stage.setTitle("Menu Inicial");
         stage.setScene(scene);
         stage.show();
     }
